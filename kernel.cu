dcd780eeec0e372baf7533f894faca6252cf40d6
Sarun Angamnuaysiri <Sarun Angamnuaysiri>
2016-07-11 10:45:55 +0700

    Full Test

--- kernel.cu
+++ kernel.cu
@@ -14,45 +14,30 @@
 
 using namespace std;
 
-void make_kernel(int f1, int f2, double *kernel){
-	double val1, val2, sum = 0;
-	for (int d1 = 1; d1 <= f1; d1++)
+void multi(double *out, double *a, double *b, const int Ra, const int Rb, const int Ca, const int Cb);
+void subMaxtrix(double *out, double *a, double *b, const int R, const int C);
+double sumMaxtrix(double *in, const int R, const int C);
+void nonlocalmean(const int t1, const int t2, const int f1, const int f2, const int Wx, const int Wy, double *in, double *out, int h);
+
+void writeFile(const char *filename, const int size, double* readArray)
+{
+	//const char *filename = "D:\\save.dat";
+	ofstream output(filename, std::ios::binary | std::ios::out);
+
+
+
+	for (int i = 0; i < size; i++)
 	{
-		for (int d2 = 1; d2 <= f2; d2++)
-		{
-			val1 = 1 / powf((2 * d1 + 1), 2);
-			val2 = 1 / powf((2 * d2 + 1), 2);
-			for (int i = 0 - d1; i <= d1; i++)
-			{
-				for (int j = 0 - d2; j <= d2; j++)
-				{
-					kernel[f2 - j + ((f1 - i)*(2 * f2 + 1))] += val1 + val2;
-				}
-			}
-		}
+		//file.read( (char*)(&aDouble), sizeof( double ) );  
+		//readArray[m + (n*SIGNAL_SIZE) + (i*SIGNAL_SIZE*CHANNEL)] = aDouble;
+		output.write((char *)&readArray[i], sizeof(double));
+
 	}
-	for (int i = 0; i < 2 * f1 + 1; i++)
-	{
-		for (int j = 0; j < 2 * f2 + 1; j++)
-		{
-			kernel[j + (i * (2 * f2 + 1))] /= (f1 + f2 / 2.0);
-			sum += kernel[j + (i * (2 * f2 + 1))];
-		}
-	}
-	for (int i = 0; i < 2 * f1 + 1; i++)
-	{
-		for (int j = 0; j < 2 * f2 + 1; j++)
-		{
-			kernel[j + (i * (2 * f2 + 1))] /= sum;
-		}
-	}
+
+	output.close();
 }
 
-__global__ void gpu_make_kernel(int f1, int f2, double *kernel){
-	const int nThdx = blockDim.x * gridDim.x;
-	const int nThdy = blockDim.y * gridDim.y;
-	const int tIDx = blockIdx.x * blockDim.x + threadIdx.x;
-	const int tIDy = blockIdx.y * blockDim.y + threadIdx.y;
+void make_kernel(int f1, int f2, double *kernel){
 	double val1, val2, sum = 0;
 	for (int d1 = 1; d1 <= f1; d1++)
 	{
@@ -154,7 +139,7 @@ void padarray(double *in, double *out, const int f1, const int f2, const int Wx,
 		-----------
 		-    3    -
 		-----------
-		 1 | X | 2
+		 1 | X | 2		
 		-----------
 		-    4    -
 		-----------
@@ -207,137 +192,200 @@ void main()
 {
 	const int f1 = 3;
 	const int f2 = 3;
-	const int Wx = 5;
-	const int Wy = 5;
-	
-	double *in = new double[Wx * Wy];
-	double *out = new double[(Wx + (2 * f1))*(Wy + (2 * f2))];
-	
-	for (int i = 0; i < Wx;i++)
-	{
-		for (int j = 0; j < Wy; j++)
-			in[j + (i * Wx)] = 5 + (i*j);
-	}
+	const int t1 = 5;
+	const int t2 = 5;
+	const int Wx = 81;
+	const int Wy = 8192;
+	const int h  = 15;
 
-	double *d_in;
-	double *d_out;
-
-	cudaMalloc((void **)&d_in, Wx*Wy*sizeof(double));
-	cudaMalloc((void **)&d_out, (Wx+2*f1)*(Wy+2*f2)*sizeof(double));
-	cudaMemcpy(d_in, in, Wx*Wy*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemset(d_out, 0, (Wx + 2 * f1)*(Wy + 2 * f2)*sizeof(double));
-
-	gpu_padarray <<<1024,1024>>> (d_in, d_out, f1, f2, Wx, Wy);
-	cudaMemcpy(out, d_out, (Wx + 2 * f1)*(Wy + 2 * f2)*sizeof(double), cudaMemcpyDeviceToHost);
-
-	cout << endl;
-	for (int i = 0; i < Wx + 2 * f1; i++)
-	{
-		for (int j = 0; j < Wy + 2 * f2; j++)
-		{
-			printf("%d\t", j + (i * (Wx + (2 * f1))));
-		}
-		cout << endl;
-	}
-
-	cout << endl;
-	for (int i = 0; i < Wx + 2 * f1; i++)
-	{
-		for (int j = 0; j < Wy + 2 * f2; j++)
-		{
-			printf("%lf\t", out[j + (i * (Wx + (2 * f1)))]);
-		}
-		cout << endl;
-	}
-	cout << endl;
-
-	double *kernel = new double[(2 * f1 + 1) * (2 * f2 + 1)];
-	double *d_kernel;
+	double *kernel  = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *kernel0 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
 	for (int i = 0; i < (2 * f1 + 1)*(2 * f2 + 1); i++)
-		kernel[i] = 0;
+		kernel[i] = 0; // Fill zero in kernel
 
-	cudaMalloc((void **)&d_kernel, (2 * f1 + 1) * (2 * f2 + 1) * sizeof(double));
-	cudaMemset(d_kernel, 0, (2 * f1 + 1) * (2 * f2 + 1) * sizeof(double));
-
-	//gpu_make_kernel << <(32,32), (32,32) >> >(f1, f2, d_kernel);
-	//cudaMemcpy(kernel, d_kernel, (2 * f1 + 1) * (2 * f2 + 1) * sizeof(double), cudaMemcpyDeviceToHost);
+	for (int i = 0; i < (2 * f1 + 1)*(2 * f2 + 1); i++)
+		kernel0[i] = 9; // Fill zero in kernel
 
 	make_kernel(f1, f2, kernel);
+
 	for (int i = 0; i < (2 * f1 + 1)*(2 * f2 + 1); i++)
 	{
 		if (i % (2 * f2 + 1) == 0) printf("\n");
 		printf("%lf ", kernel[i]);
 	}
+
+	// * matic
+	//multi(kernel0, kernel, kernel, (2 * f1 + 1), (2 * f2 + 1), (2 * f1 + 1), (2 * f2 + 1)); // -> before test bug 
+	//subMaxtrix(kernel0, kernel0, kernel, (2 * f1 + 1), (2 * f2 + 1));
+	//double x = sumMaxtrix(kernel, (2 * f1 + 1), (2 * f2 + 1)); 
+	//cout << "AAA" << x;
+	double *A = new double[Wx*Wy];
+	for (int l = 0; l < Wx*Wy; l++)
+	{
+		//if (l%Wx == 0) cout << endl;
+		A[l] = l*(rand() % 100 / 100.0);
+		//printf("%lf\t", A[l]);
+	}
+
+	double *B = new double[Wx*Wy];
+	for (int l = 0; l < Wx*Wy; l++)
+		B[l] = 0;
+	
+	cout << endl;
+	nonlocalmean(t1, t2, f1, f2, Wx, Wy, A, B, h);
+	writeFile("D://A.dat", 8192 * 81, A);
+	writeFile("D://B.dat", 8192 * 81, B);
 }
 
-void nonlocalmean(const int t1, const int t2, const int f1, const int f2, const int Wx, const int Wy) // Dev
+void multi(double *out, double *a, double *b, const int Ra, const int Rb, const int Ca, const int Cb)
 {
-	double **w1 = new double *[2 * f1 + 1];
-	for (int i = 0; i < 2 * f1 + 1; i++)
-		w1[i] = new double[2 * f2 + 1];
-
-	double **w2 = new double *[2 * f1 + 1];
-	for (int i = 0; i < 2 * f1 + 1; i++)
-		w2[i] = new double[2 * f2 + 1];
-
-	double **input2;
-
-	int i1, j1;
-	for (int i = 0; i < Wx; i++)
+	for (int i = 0; i < Ra; i++)
 	{
-		for (int j = 0; j < Wy; j++)
+		for (int j = 0; j < Ca; j++)
 		{
+			for (int k = 0; k < Cb; k++)
+				out[j + (i * Cb)] += a[k + i*Ca] * b[j + k*Cb];
+		}
+	}
+	
+	//display output
+	/*
+	for (int i = 0; i < Ra; i++)
+	{
+		for (int j = 0; j < Cb; j++)
+		{
+			printf("%lf\t", out[j + (i*Cb)]);
+		}
+		cout << endl ;
+	}
+	*/
+}
 
-			i1 = i + f1;
-			j1 = j + f2;
+void subMaxtrix(double *out, double *a, double *b, const int R, const int C)
+{
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			out[j + (i*C)] = a[j + (i*C)] - b[j + (i*C)];
+		}
+	}
 
-			//W1 = input2(i1 - f1:i1 + f1, j1 - f2 : j1 + f2);
-			int Ln = i1 + f1;
-			for (int tmp1 = 0; tmp1 < 2 * f1 + 1; tmp1++)
+	//display output
+	/*
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			printf("%lf\t", out[j + (i*C)]);
+		}
+		cout << endl;
+	}
+	*/
+}
+
+double sumMaxtrix(double *in, const int R, const int C)
+{
+	double sum = 0;
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			sum += in[j + (i*C)];
+		}
+	}
+	return sum;
+}
+
+void nonlocalmean(const int t1, const int t2, const int f1, const int f2, const int Wx, const int Wy, double *in, double *out, int h) // Dev
+{
+	double *padout = new double[(2 * f1 + Wx) * (2 * f2 + Wy)];
+	
+	double *kernel = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *W1 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *W2 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *temp0 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *temp1 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+	double *temp2 = new double[(2 * f1 + 1) * (2 * f2 + 1)];
+
+	for (int i = 0; i < (2 * f1 + 1)*(2 * f2 + 1); i++)
+		kernel[i] = 0; // Fill zero in kernel
+
+	int i1, j1, count=0;
+	double wmax = 0;
+	double average = 0;
+	double sweight = 0;
+	double d, w;
+	int rmin, rmax, smin, smax ;
+	gpu_padarray <<<1,128>>> (in, padout, f1, f2, Wx, Wy);
+	make_kernel(f1, f2, kernel);
+	h = h*h;
+
+	for (int i = 0; i < Wy; i++)
+	{
+		printf("%d ", i);
+		for (int j = 0; j < Wx; j++)
+		{
+			//printf("A");
+			i1 = i + f1 -1;
+			j1 = j + f2 -1;
+
+			//W1= input2(i1-f1:i1+f1 , j1-f2:j1+f2);
+			count = 0;
+			for (int tmp1 = i1 - f1; tmp1 < i1 + f1; tmp1++)
 			{
-				int Lm = j1 - f2;
-				for (int tmp2 = 0; tmp2 < 2 * f2 + 1; tmp2++)
+				for (int tmp2 = j1 - f2; tmp2 < j1 + f2; tmp2++)
 				{
-					w1[tmp1][tmp2] = input2[Ln][Lm++];
+					//printf("%d\n", tmp2);
+					W1[count++] = padout[tmp2 + (tmp1 * (2 * f2 + 1))];
+
 				}
-				Ln++;
 			}
 
-			double wmax = 0;
-			double average = 0;
-			double sweight = 0;
-			/*
-			rmin = max(i1-t1,f1+1);
-			rmax = min(i1+t1,m+f1);
-			smin = max(j1-t2,f2+1);
-			smax = min(j1+t2,n+f2);
-			*/
-			double rmin = (i1 - t1>f1 + 1) ? i1 - t1 : f1 + 1  ;
-			double rmax = (i1 - t1>f1 + 1) ? f1 + 1  : i1 - t1 ;
-			double smin = (j1 - t2>f2 + 1) ? j1 - t2 : f2 + 1  ;
-			double smax = (j1 - t2>f2 + 1) ? f2 + 1  : j1 - t2 ;
+			wmax = 0;
+			average = 0;
+			sweight = 0;
 
-			for (int r = rmin; r <= rmax; r++)
+			rmin = (i1 - t1>f1 + 1) ? i1 - t1 : f1 + 1;
+			rmax = (i1 + t1 < Wy + f1) ? i1 + t1 : Wx + f1;
+			smin = (j1 - t2>f2 + 1) ? j1 - t2 : f2 + 1;
+			smax = (j1 + t2 < Wx + f2) ? j1 + t2 : Wx + f2;
+
+			for (int r = rmin; r < rmax; r++)
 			{
-				for (int s = smin; r <= smax; r++)
+				for (int s = smin; s < smax; s++)
 				{
 					if (r == i1 && s == j1) continue;
 
 					//W2= input2(r-f1:r+f1 , s-f2:s+f2);
-					int Ln2 = r - f1;
-					for (int tmp1 = 0; tmp1 < 2 * f1 + 1; tmp1++)
+					count = 0;
+					for (int tmp1 = r - f1; tmp1 < r + f1; tmp1++)
 					{
-						int Lm2 = s - f2;
-						for (int tmp2 = 0; tmp2 < 2 * f2 + 1; tmp2++)
+						for (int tmp2 = s - f2; tmp2 < s + f2; tmp2++)
 						{
-							w2[tmp1][tmp2] = input2[Ln2][Lm2++];
+							W2[count++] = padout[tmp2 + (tmp1 * (2 * f2 + 1))];
 						}
-						Ln2++;
 					}
 
+					//d = sum(sum(kernel.*(W1-W2).*(W1-W2)));
+					subMaxtrix(W1, W1, W2, (2 * f1 + 1), (2 * f2 + 1)); //W1 = W1-W2
+					multi(temp0, kernel, W1, (2 * f1 + 1), (2 * f2 + 1), (2 * f1 + 1), (2 * f2 + 1)); //temp = kernel * (W1-w2)
+					multi(temp1, temp0, W1, (2 * f1 + 1), (2 * f2 + 1), (2 * f1 + 1), (2 * f2 + 1)); //temp = kernel * (W1-w2) * (W1-W2)
+					d = sumMaxtrix(temp1, (2 * f1 + 1), (2 * f2 + 1));
+					w = exp(0-d / h);
+					if (w>wmax) wmax = w;
+					sweight = sweight + w;
+					average = average + w * padout[s + r*(2 * f2 + 1)];
 				}
 			}
 
+			average = average + wmax*padout[j1 + i1*(2 * f2 + 1)];
+			sweight = sweight + wmax;
+
+			if (sweight > 0)
+				out[j + i*Wx] = average / sweight;
+			else
+				out[j + i*Wx] = in[j + i*Wx];
 		}
 	}
 }
